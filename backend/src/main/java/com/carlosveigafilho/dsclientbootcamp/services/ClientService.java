f4ec80129231992e347bc9cc261311d13d454143
carlos5veiga <carlos5veiga@gmail.com>
2022-01-25 14:13:54 -0300

    Database seeding

--- backend/src/main/java/com/carlosveigafilho/dsclientbootcamp/services/ClientService.java
+++ backend/src/main/java/com/carlosveigafilho/dsclientbootcamp/services/ClientService.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.carlosveigafilho.dsclientbootcamp.entities.Client;
 import com.carlosveigafilho.dsclientbootcamp.repositories.ClientRepository;
@@ -15,8 +16,8 @@ public class ClientService {
 	@Autowired
 	private ClientRepository repository;
 
+	@Transactional(readOnly=true)
 	public List<Client> findAll(){
-		System.out.println(Instant.now());
 		return repository.findAll();
 	}
 }
