bc2ae8b7cdee7a5a20b1712ba7f70a80b7b9b6cc
carlos5veiga <carlos5veiga@gmail.com>
2022-01-25 12:08:56 -0300

    Project created

---
+++ backend/src/main/java/com/carlosveigafilho/dsclientbootcamp/DsclientbootcampApplication.java
@@ -0,0 +1,13 @@
+package com.carlosveigafilho.dsclientbootcamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsclientbootcampApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsclientbootcampApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/carlosveigafilho/dsclientbootcamp/DsclientbootcampApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlosveigafilho.dsclientbootcamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsclientbootcampApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
