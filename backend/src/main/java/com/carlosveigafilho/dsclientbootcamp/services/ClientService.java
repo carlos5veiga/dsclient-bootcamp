5d00a19f0d61a69f2da1c6e968eb25635bfe693c
carlos5veiga <carlos5veiga@gmail.com>
2022-01-25 17:13:53 -0300

    Post new client

--- backend/src/main/java/com/carlosveigafilho/dsclientbootcamp/services/ClientService.java
+++ backend/src/main/java/com/carlosveigafilho/dsclientbootcamp/services/ClientService.java
@@ -25,10 +25,26 @@ public class ClientService {
 		return list.stream().map(obj -> new ClientDTO(obj)).collect(Collectors.toList());
 	}
 
-	@Transactional
+	@Transactional(readOnly=true)
 	public ClientDTO findById(Long id) {
 		Optional<Client> obj = repository.findById(id);
 		Client entity = obj.orElseThrow(() -> new ObjectNotFoundException("Id not found: " + id));
 		return new ClientDTO(entity);
+	}
+
+	@Transactional
+	public ClientDTO insert(ClientDTO dto) {
+		Client entity = new Client();
+		copyDtoToEntity(entity, dto);
+		entity = repository.save(entity);
+		return new ClientDTO(entity);
+	}
+
+	private void copyDtoToEntity(Client entity, ClientDTO dto) {
+		entity.setName(dto.getName());
+		entity.setCpf(dto.getCpf());
+		entity.setIncome(dto.getIncome());
+		entity.setBirthDate(dto.getBirthDate());
+		entity.setChildren(dto.getChildren());
 	}
 }
